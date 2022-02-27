0b46eecc29c740a25d60a88b767ada7bc496ba82
wjswhdgus123 <wjswhdgus123@naver.com>
2022-02-27 14:00:46 +0900

    2022_02_28

--- bash/git.sh
+++ bash/git.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
-function GitStting {
 GitUSER='wjswhdgus123'
 GitEMAIL='wjswhdgus123@naver.com'
 export GitDIR='/SOURCE'
 GitSSH='git@github.com:wjswhdgus123/jjhrepo.git'
 Repo=`echo $GitSSH|awk -F '/' '{print $2}'|sed s/.git//g`
 ScriptsDIR=bash
+
+function GitStting {
 mkdir -p $GitDIR
 git config --global user.name $GitUSER
 git config --global user.email $GitEMAIL
 cd $GitDIR
 git init
-#git remote add origin $GitSSH
+git remote add origin $GitSSH
 git clone $GitSSH
 }
 
 GitStting
 cd $GitDIR/$Repo/$ScriptsDIR
+pwd
 sh Pkg_install.sh
 
