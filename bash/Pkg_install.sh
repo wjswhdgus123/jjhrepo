268bf29f3fbd9377cbd14660d080f70629df54e9
wjswhdgus123 <wjswhdgus123@naver.com>
2022-02-28 15:08:25 +0900

    2022_02_28

---
+++ bash/Pkg_install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+function PKG_INSTALL {
+LISTDIR=$GitDIR/bash/pkglist
+dnf install -y epel-release 
+dnf install -y sysstat net-tool fping curl wget bind-utils net-snmp net-snmp-utils strace lsof 
+
+if [ ! -e $LISTDIR -o "-s" "$LISTDIR" ]; then 
+
+echo 
+else
+
+for PKG in `cat $LISTDIR`
+do
+dnf -y install $PKG
+done
+
+fi
+
+}
--- bash/git.sh
+++ bash/git.sh
@@ -2,8 +2,10 @@
 function GitStting {
 GitUSER='wjswhdgus123'
 GitEMAIL='wjswhdgus123@naver.com'
-GitDIR='/SOURCE'
+export GitDIR='/SOURCE'
 GitSSH='git@github.com:wjswhdgus123/jjhrepo.git'
+Repo=`echo $GitSSH|awk -F '/' '{print $2}'|sed s/.git//g`
+ScriptsDIR=bash
 mkdir -p $GitDIR
 git config --global user.name $GitUSER
 git config --global user.email $GitEMAIL
@@ -12,4 +14,8 @@ git init
 #git remote add origin $GitSSH
 git clone $GitSSH
 }
+
+GitStting
+cd $GitDIR/$Repo/$ScriptsDIR
+sh Pkg_install.sh
 
