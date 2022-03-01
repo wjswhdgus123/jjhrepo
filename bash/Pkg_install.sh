7e22d72ea352d5191eeae8e7b1496b7b38f266ba
wjswhdgus123 <wjswhdgus123@naver.com>
2022-03-01 10:19:36 +0900

    2022_0301

--- bash/Pkg_install.sh
+++ bash/Pkg_install.sh
@@ -4,8 +4,8 @@ function PKG_INSTALL {
 LISTDIR=$GitDIR/bash/pkglist
 #LISTDIR=/root/
 dnf install -y epel-release 
-dnf install -y sysstat net-tools fping curl wget bind-utils net-snmp net-snmp-utils strace lsof vim chrony
-
+dnf install -y sysstat net-tools fping curl wget bind-utils net-snmp net-snmp-utils strace lsof vim chrony make cmake
+dnf install -y python39
 if [ ! -e $LISTDIR -o "-s" "$LISTDIR" ]; then 
 
 echo 
@@ -18,6 +18,23 @@ done
 
 fi
 
+dnf -y update 
 }
+
+function Pkg_GeoIP {
+PKGDIR=$GitDIR/PKG
+echo $PKGDIR
+mkdir -p $PKGDIR
+dnf install -y  gcc gcc-c++ make automake unzip zip kernel-devel-`uname -r` iptables-devel perl-CPAN libmnl* perl-NetAddr-IP perl-Text-CSV_XS
+wget http://downloads.sourceforge.net/project/xtables-addons/Xtables-addons/xtables-addons-2.13.tar.xz  -P $PKGDIR
+cd $PKGDIR
+tar xvzf xtables-addons-2.13.tar.xz
+cd xtables-addons-2.13
+sed -i s/build_TARPIT=m/#build_TARPIT=m/g mconfig
+./configure
+make;make install
+
+}
+
 
 PKG_INSTALL
--- bash/git.sh
+++ bash/git.sh
@@ -16,6 +16,19 @@ git remote add origin $GitSSH
 git clone $GitSSH
 }
 
+function GeoGit{
+Lic='g0Vcw6gp6PHPSYfe'
+GeoDIR='GeoLite2xtables'
+cd $GitDIR
+git clone https://github.com/mschmitt/GeoLite2xtables
+pwd
+cd $GeoDIR
+
+mv geolite2.license.example geolite2.license
+sed -i s/asdfqwer1234/$Lic/g geolite2.license
+}
+
+
 GitStting
 cd $GitDIR/$Repo/$ScriptsDIR
 pwd
